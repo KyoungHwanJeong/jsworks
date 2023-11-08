c1c87bb168d050f68496f1433175a18eae3c8693
KyoungHwanJeong <jkh3100@naver.com>
2023-11-08 17:52:41 +0900

    javascript

---
+++ day1/js/op.js
@@ -0,0 +1,56 @@
+// op.js
+let num1 = 8, num2 = 5;
+let result;
+// 산술 연산
+/*
+result = num1 + num2;
+console.log(result);
+
+result = num1 - num2;
+console.log(result);
+
+result = num1 * num2;
+console.log(result);
+
+result = num1 / num2;
+console.log(result);
+
+result = num1 % num2;
+console.log(result);
+*/
+
+// 비교 연산
+result = num1 > num2;
+console.log(result);          // true
+console.log(typeof(result));  // boolean
+
+result = num1 == num2;        // false
+console.log(result);
+
+result = num1 != num2;        // true
+console.log(result);
+
+// 논리 연산
+// 논리 곱 - and
+result = (num1 == num2) && (num1 >= num2);  // false
+console.log(result);
+
+result = (num1 == num2) || (num1 >= num2);  // true
+console.log(result);
+
+// 논리 부정 - not
+result = !(num1 == num2)
+console.log(result);
+
+// 조건 연산자
+result = (num1 < num2) ? 'T' : 'F';       // 'F'
+console.log(result);
+
+// 짝수/홀수 판정
+result = (num1 % 2 == 0) ? "짝수" : "홀수";
+console.log(result);
+// alert은 window 소속이지만 생략해도 된다
+//window.alert("판정 결과: " + result)// 실행될 때 "짝수"
+// 안쪽에는 작은 ' ' 따옴표
+document.write("<span class='acess'>판정 결과: " + 
+        result + "</span>");
---
+++ day1/js/prompt1.js
@@ -0,0 +1,12 @@
+// prompt1.js
+// 입력한 숫자에 10을 더하는 프로그램
+// parseInt("문자") - 문자형을 숫자로 변환함 
+/*
+let num = prompt("숫자를 입력하세요: ", "1");
+num = parseInt(num) + 10; // 입력한 숫자를 받아서 10을 더함
+document.write("입력한 숫자는: " + num + "입니다.");
+*/
+
+// 이름을 입력받는 프로그램
+let yourName = prompt("이름을 입력하세요");
+document.write("<span class = 'access'>" + yourName + "</span> 님 환영합니다.");
