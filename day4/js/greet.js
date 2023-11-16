adabf5ee5e9add509354889caeb5fbe987bcae58
KyoungHwanJeong <jkh3100@naver.com>
2023-11-16 11:33:31 +0900

    random

---
+++ day4/js/greet.js
@@ -0,0 +1,28 @@
+// greet.js
+//window.onload = function() 창이 열리면 가장 먼저 실행된다
+window.onload = function(){
+let message = ["오늘도 좋은 하루!!", "행운을 빌어요",
+                      "잘 지내니?"];
+
+
+// 인사하기 밑에 message 띄우기
+document.getElementById("demo").innerHTML = message[1];
+
+//2초 간격으로 인사말이 변경되게 함
+//콜백함수 기능 - 자기가 부르는 재귀적 용법
+//배열의 인덱스가 1증가하면 인사말이 바뀐다
+let msgIdx = 0;
+
+myGreeting();   //함수 호출
+
+function myGreeting(){
+  let msg = document.getElementById("demo")
+  msg.innerHTML = message[msgIdx];
+  msg.style.color = "blue";
+  msgIdx++;   //1증가
+  if(msgIdx == message.length){  // 인덱스 번호 제한
+    msgIdx = 0;
+  }
+  setTimeout(myGreeting, 2000);   //myGreating() 호출
+}
+}
---
+++ day4/js/time.js
@@ -0,0 +1,21 @@
+// time.js
+//시계 생성
+// setInterval이 window 함수여서 먼저 실행된다
+setInterval(myWatch, 1000);
+
+function myWatch(){
+  const now = new Date(); //날짜와 시간 함께 생성
+  let time = now.toLocaleTimeString(); //시간만 출력하기
+  console.log(time);
+
+  let watch = document.getElementById("show");
+  // watch.innerHTML = time;
+  watch.style.color = "blue";
+
+  // 오전, 오후 나누기
+  if(time >= 12){
+    watch.innerHTML = time.replace("오전", "am");
+  }else{
+    watch.innerHTML = time.replace("오후", "pm");
+  }
+ }
